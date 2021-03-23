ccf26cfd040721c5c1831f514cb1caa373a1ce22
Miguel ORTEGA <miguel.ortega@mythalesgroup.io>
2021-03-23 12:11:57 +0100

    Update TP5 - clean up angular app as static resource
    Préconiser des injections par constructeurs

--- todo-project-TP5/src/main/java/com/thales/formation/mapper/TodoMapper.java
+++ todo-project-TP5/src/main/java/com/thales/formation/mapper/TodoMapper.java
@@ -3,6 +3,7 @@ package com.thales.formation.mapper;
 import java.util.List;
 
 import org.mapstruct.DecoratedWith;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
@@ -11,22 +12,22 @@ import org.mapstruct.ReportingPolicy;
 import com.thales.formation.dto.TodoDto;
 import com.thales.formation.model.Todo;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 @DecoratedWith(TodoMapperDecorator.class)
 public interface TodoMapper {
-	
-	// CREATE
-	@Mapping(target = "id", ignore = true )
-	@Mapping(target = "status", ignore = true)
-	Todo dtoToModel(TodoDto todoDto);
-	
-	// UPDATE
-	@Mapping(target = "id", ignore = true)
-	@Mapping(target = "status", ignore = true)
-	void dtoToModel(TodoDto todoDto, @MappingTarget Todo todo);
-	
-	TodoDto modelToDto(Todo todo);
-	
-	List<TodoDto> modelToDto(Iterable<Todo> todo);
+
+  // CREATE
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "status", ignore = true)
+  Todo dtoToModel(TodoDto todoDto);
+
+  // UPDATE
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "status", ignore = true)
+  void dtoToModel(TodoDto todoDto, @MappingTarget Todo todo);
+
+  TodoDto modelToDto(Todo todo);
+
+  List<TodoDto> modelToDto(Iterable<Todo> todo);
 
 }
