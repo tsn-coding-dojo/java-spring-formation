2996f022564be2aff2f85829bd05ec40e8283796
Miguel ORTEGA <miguel.ortega@mythalesgroup.io>
2021-03-23 17:15:55 +0100

    Update TP6
    - Angular app as static resource
    - Add example with H2 database (embedded + file)

--- todo-project-TP6/src/main/java/com/thales/formation/controller/TodoController.java
+++ todo-project-TP6/src/main/java/com/thales/formation/controller/TodoController.java
@@ -3,7 +3,6 @@ package com.thales.formation.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,42 +16,54 @@ import com.thales.formation.service.TodoService;
 @RestController
 @RequestMapping("/api/todos")
 public class TodoController {
-	
-	@Autowired
-	private TodoService todoService;
-	
-	@Autowired
-	private TodoMapper todoMapper;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	public List<TodoDto> findAll() {
-		return todoMapper.modelToDto(todoService.findAllNotCompleted());
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/")
-	public TodoDto create(@RequestBody(required = true) TodoDto todoDto) {
-		return todoMapper.modelToDto(todoService.create(todoDto));
-	}
-	
-	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-	public void update(@PathVariable Long id, @RequestBody(required = true) TodoDto todoDto) {
-		todoDto.setId(id);
-		todoService.update(todoDto);
-	}
-	
-	@RequestMapping(method = RequestMethod.POST, value = "/{id}/complete")
-	public void complete(@PathVariable Long id) {
-		todoService.complete(id);
-	}
-	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-	public void delete(@PathVariable Long id) {
-		todoService.delete(id);
-	}
-	
-	@RequestMapping(method = RequestMethod.DELETE, value = "/")
-	public void deleteAll() {
-		todoService.deleteAll();
-	}
-	
+  private final TodoService todoService;
+
+  private final TodoMapper todoMapper;
+
+  public TodoController(TodoService todoService, TodoMapper todoMapper) {
+    super();
+    this.todoService = todoService;
+    this.todoMapper = todoMapper;
+  }
+
+  @RequestMapping(method = RequestMethod.GET, value = "/")
+  public List<TodoDto> findAll() {
+    return todoMapper.modelToDto(todoService.findAllNotCompleted());
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/")
+  public TodoDto create(@RequestBody(required = true) TodoDto todoDto) {
+    return todoMapper.modelToDto(todoService.create(todoDto));
+  }
+
+  @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+  public void update(@PathVariable Long id, @RequestBody(required = true) TodoDto todoDto) {
+    todoDto.setId(id);
+    todoService.update(todoDto);
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/{id}/complete")
+  public void complete(@PathVariable Long id) {
+    todoService.complete(id);
+  }
+
+  @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+  public void delete(@PathVariable Long id) {
+    todoService.delete(id);
+  }
+
+  @RequestMapping(method = RequestMethod.DELETE, value = "/")
+  public void deleteAll() {
+    todoService.deleteAll();
+  }
+
+  public TodoService getTodoService() {
+    return todoService;
+  }
+
+  public TodoMapper getTodoMapper() {
+    return todoMapper;
+  }
+
 }
--- todo-project-TP6/src/main/java/com/thales/formation/service/TodoService.java
+++ todo-project-TP6/src/main/java/com/thales/formation/service/TodoService.java
@@ -2,7 +2,6 @@ package com.thales.formation.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thales.formation.dto.TodoDto;
@@ -13,44 +12,48 @@ import com.thales.formation.repository.TodoRepository;
 
 @Service
 public class TodoService {
-	
-	@Autowired
-	private TodoMapper todoMapper;
-	
-	@Autowired
-	private TodoRepository todoRepository;
-	
-	public Iterable<Todo> findAllNotCompleted() {
-		return todoRepository.findByStatus(TodoStatus.TODO);
-	}
-	
-	public Todo findById(Long id) {
-		Optional<Todo> optTodo = todoRepository.findById(id);
-		return optTodo.get();
-	}
 
-	public Todo create(TodoDto todoDto) {
-		Todo todo = todoMapper.dtoToModel(todoDto);
-		todo.setStatus(TodoStatus.TODO);
-		return todoRepository.save(todo);
-	}
+  private final TodoMapper todoMapper;
 
-	public void update(TodoDto todoDto) {
-		Todo todo = this.findById(todoDto.getId());
-		todo.setName(todoDto.getName());
-	}
-	
-	public void complete(Long todoId) {
-		Todo todo = this.findById(todoId);
-		todo.setStatus(TodoStatus.COMPLETED);
-	}
+  private final TodoRepository todoRepository;
 
-	public void delete(Long id) {
-		todoRepository.deleteById(id);
-	}
-	
-	public void deleteAll() {
-		todoRepository.deleteAll();
-	}
+  public TodoService(TodoMapper todoMapper, TodoRepository todoRepository) {
+    super();
+    this.todoMapper = todoMapper;
+    this.todoRepository = todoRepository;
+  }
+
+  public Iterable<Todo> findAllNotCompleted() {
+    return todoRepository.findByStatus(TodoStatus.TODO);
+  }
+
+  public Todo findById(Long id) {
+    Optional<Todo> optTodo = todoRepository.findById(id);
+    return optTodo.get();
+  }
+
+  public Todo create(TodoDto todoDto) {
+    Todo todo = todoMapper.dtoToModel(todoDto);
+    todo.setStatus(TodoStatus.TODO);
+    return todoRepository.save(todo);
+  }
+
+  public void update(TodoDto todoDto) {
+    Todo todo = this.findById(todoDto.getId());
+    todo.setName(todoDto.getName());
+  }
+
+  public void complete(Long todoId) {
+    Todo todo = this.findById(todoId);
+    todo.setStatus(TodoStatus.COMPLETED);
+  }
+
+  public void delete(Long id) {
+    todoRepository.deleteById(id);
+  }
+
+  public void deleteAll() {
+    todoRepository.deleteAll();
+  }
 
 }
