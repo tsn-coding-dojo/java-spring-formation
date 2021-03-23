5d2a9883928ed268d2f982c0b8c5ff06da3704b2
Miguel ORTEGA <miguel.ortega@mythalesgroup.io>
2021-03-23 17:51:09 +0100

    Update TP7
    - angular app as static resource
    - Add commented example for exception

--- todo-project-TP7/src/main/java/com/thales/formation/service/TodoService.java
+++ todo-project-TP7/src/main/java/com/thales/formation/service/TodoService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thales.formation.dto.TodoDto;
 import com.thales.formation.enums.TodoStatus;
@@ -12,45 +13,57 @@ import com.thales.formation.model.Todo;
 import com.thales.formation.repository.TodoRepository;
 
 @Service
+//@Transactional
 public class TodoService {
-	
-	@Autowired
-	private TodoMapper todoMapper;
-	
-	@Autowired
-	private TodoRepository todoRepository;
-	
-	public Iterable<Todo> findAllNotCompleted() {
-		return todoRepository.findByStatus(TodoStatus.TODO);
-	}
-	
-	public Todo findById(Long id) {
-		Optional<Todo> optTodo = todoRepository.findById(id);
-		return optTodo.get();
-	}
 
-	public Todo create(TodoDto todoDto) {
-		Todo todo = todoMapper.dtoToModel(todoDto);
-		todo.setStatus(TodoStatus.TODO);
-		return todoRepository.save(todo);
-	}
+  @Autowired
+  private TodoMapper todoMapper;
 
-	public void update(TodoDto todoDto) {
-		Todo todo = this.findById(todoDto.getId());
-		todo.setName(todoDto.getName());
-	}
-	
-	public void complete(Long todoId) {
-		Todo todo = this.findById(todoId);
-		todo.setStatus(TodoStatus.COMPLETED);
-	}
+  @Autowired
+  private TodoRepository todoRepository;
 
-	public void delete(Long id) {
-		todoRepository.deleteById(id);
-	}
-	
-	public void deleteAll() {
-		todoRepository.deleteAll();
-	}
+  public Iterable<Todo> findAllNotCompleted() {
+    return todoRepository.findByStatus(TodoStatus.TODO);
+  }
+
+  public Todo findById(Long id) {
+    Optional<Todo> optTodo = todoRepository.findById(id);
+    return optTodo.get();
+  }
+
+  //  @Transactional
+  public Todo create(TodoDto todoDto) {
+    Todo todo = todoMapper.dtoToModel(todoDto);
+    todo.setStatus(TodoStatus.TODO);
+
+    //    Todo save = todoRepository.save(todo);
+    //    if (save != null) {
+    //      throw new RuntimeException("oups!");
+    //    }
+    //    return save;
+    return todoRepository.save(todo);
+  }
+
+  @Transactional
+  public void update(TodoDto todoDto) {
+    Todo todo = this.findById(todoDto.getId());
+    todo.setName(todoDto.getName());
+
+    //    throw new RuntimeException("oups!");
+  }
+
+  @Transactional
+  public void complete(Long todoId) {
+    Todo todo = this.findById(todoId);
+    todo.setStatus(TodoStatus.COMPLETED);
+  }
+
+  public void delete(Long id) {
+    todoRepository.deleteById(id);
+  }
+
+  public void deleteAll() {
+    todoRepository.deleteAll();
+  }
 
 }
