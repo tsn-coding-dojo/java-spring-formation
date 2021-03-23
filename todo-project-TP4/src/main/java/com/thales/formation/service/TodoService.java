79604219a0061c63cdd0b4ed1845d6099f8f8c01
Miguel ORTEGA <miguel.ortega@mythalesgroup.io>
2021-03-23 11:55:00 +0100

    Update TP4
    - Compile Angular App as static image
    - Test lombok is still working

--- todo-project-TP4/src/main/java/com/thales/formation/service/TodoService.java
+++ todo-project-TP4/src/main/java/com/thales/formation/service/TodoService.java
@@ -13,45 +13,47 @@ import com.thales.formation.model.Todo;
 
 @Service
 public class TodoService {
-	
-	private Map<Long, Todo> todos = new HashMap<>();
-	private AtomicLong atomicLong = new AtomicLong();
-	
-	public Iterable<Todo> findAllNotCompleted() {
-		return todos.values().stream()
-				.filter(todos -> todos.getStatus() == TodoStatus.TODO)
-				.collect(Collectors.toList());
-	}
-	
-	public Todo findById(Long id) {
-		return todos.get(id);
-	}
 
-	public Todo create(TodoDto todoDto) {
-		Todo todo = new Todo();
-		todo.setId(atomicLong.getAndIncrement()).setName(todoDto.getName()).setStatus(TodoStatus.TODO);
-		
-		todos.put(todo.getId(), todo);
-		
-		return todo;
-	}
+  private Map<Long, Todo> todos = new HashMap<>();
 
-	public void update(TodoDto todoDto) {
-		Todo todo = this.findById(todoDto.getId());
-		todo.setName(todoDto.getName());
-	}
-	
-	public void complete(Long todoId) {
-		Todo todo = this.findById(todoId);
-		todo.setStatus(TodoStatus.COMPLETED);
-	}
+  private AtomicLong atomicLong = new AtomicLong();
 
-	public void delete(Long id) {
-		todos.remove(id);
-	}
-	
-	public void deleteAll() {
-		todos.clear();
-	}
+  public Iterable<Todo> findAllNotCompleted() {
+    return todos.values().stream()
+        .filter(todos -> todos.getStatus() == TodoStatus.TODO)
+        .collect(Collectors.toList());
+  }
+
+  public Todo findById(Long id) {
+    return todos.get(id);
+  }
+
+  public Todo create(TodoDto todoDto) {
+    Todo todo = new Todo();
+    todo.setId(atomicLong.getAndIncrement()).setName(todoDto.getName()).setStatus(TodoStatus.TODO);
+
+    todos.put(todo.getId(), todo);
+
+    return todo;
+  }
+
+  public void update(TodoDto todoDto) {
+    Todo todo = this.findById(todoDto.getId());
+
+    todo.setName(todoDto.getName());
+  }
+
+  public void complete(Long todoId) {
+    Todo todo = this.findById(todoId);
+    todo.setStatus(TodoStatus.COMPLETED);
+  }
+
+  public void delete(Long id) {
+    todos.remove(id);
+  }
+
+  public void deleteAll() {
+    todos.clear();
+  }
 
 }
