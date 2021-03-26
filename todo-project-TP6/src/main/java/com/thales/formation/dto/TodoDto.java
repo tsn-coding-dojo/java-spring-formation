4edad4e4712facad959bcc1fc42e04f45e76f635
Miguel ORTEGA <miguel.ortega@mythalesgroup.io>
2021-03-26 09:46:17 +0100

    Remove lombok

--- todo-project-TP6/src/main/java/com/thales/formation/dto/TodoDto.java
+++ todo-project-TP6/src/main/java/com/thales/formation/dto/TodoDto.java
@@ -1,15 +1,25 @@
 package com.thales.formation.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-
-@Getter
-@Setter
-@Accessors(chain=true)
 public class TodoDto {
-	
-	private Long id;
-	private String name;
+
+  private Long id;
+
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 
 }
